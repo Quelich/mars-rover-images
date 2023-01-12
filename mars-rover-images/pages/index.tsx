b08229663852eff543701a5599a91f35cd48d409
Emre Kılıç <emre1kilic@outlook.com>
2023-01-12 23:39:45 +0300

    add grid ui for photos

--- mars-rover-images/pages/index.tsx
+++ mars-rover-images/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import * as React from "react";
-import {Grid, Text } from "@nextui-org/react";
+import { Grid, Text, Card, Col } from "@nextui-org/react";
 import { GetServerSideProps } from "next";
 const NASA_API_KEY = process.env.NASA_API_KEY;
 const NASA_API_URL = "https://api.nasa.gov/mars-photos/api/v1";
@@ -28,8 +28,6 @@ interface HomePageProps {
   }[];
 }
 
-
-
 export default function Home({ data }: HomePageProps) {
   return (
     <>
@@ -44,11 +42,38 @@ export default function Home({ data }: HomePageProps) {
         <h1 className="text-3xl font-bold">Mars Rover Images</h1>
       </main>
 
-      <ul>
-        {data.photos.map(({id, sol}) => {
-          return <li key={id}>Id:{id}, Sol:{sol}</li>;
+      <Grid.Container gap={2} justify="center">
+        {data.photos.map(({ id, sol, img_src, camera, rover}) => {
+          return (
+            <Grid key={id} xs={16} sm={4}>
+              <Card>
+                <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+                  <Col>
+                    <Text
+                      size={12}
+                      weight="bold"
+                      transform="uppercase"
+                      color="#ffffffAA"
+                    >
+                      {rover.name}
+                    </Text>
+                    <Text h4 color="white">
+                      Photo Id: {id}
+                    </Text>
+                  </Col>
+                </Card.Header>
+                <Card.Image
+                  src={img_src}
+                  objectFit="cover"
+                  width="100%"
+                  height={340}
+                  alt="Card image background"
+                />
+              </Card>
+            </Grid>
+          );
         })}
-      </ul>
+      </Grid.Container>
     </>
   );
 }
@@ -56,13 +81,14 @@ export default function Home({ data }: HomePageProps) {
 export const getServerSideProps: GetServerSideProps = async () => {
   const category = "rovers";
   const rover = "curiosity";
-  const solarDay = 1;
+  const solarDay = 1000;
 
   const res = await fetch(
     `${NASA_API_URL}/${category}/${rover}/photos?sol=${solarDay}&api_key=${NASA_API_KEY}`
   );
 
   const data = await res.json();
+  console.log(data);
 
   return {
     props: { data }, // will be passed to the page component as props
