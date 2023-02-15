737f0031d654be09c34ca168877d724d97082c42
Emre Kılıç <emre1kilic@outlook.com>
2023-02-15 22:38:01 +0300

    add loading state

--- mars-rover-images/pages/index.tsx
+++ mars-rover-images/pages/index.tsx
@@ -9,14 +9,16 @@ import {
   Input,
   Dropdown,
   Button,
+  Loading,
+  Container,
 } from "@nextui-org/react";
 import { GetServerSideProps } from "next";
 
-export default function Home() {
+export default function Home({ initialRoverData }) {
   const [selectedSolarDay, setSolarDay] = useState("0");
-  const [loadedRoverData, setLoadedRoverData] = useState([]);
-
- 
+  const [solArrayLength, setSolArrayLength] = useState(0);
+  const [loadedRoverData, setLoadedRoverData] = useState([initialRoverData]);
+  const [isLoading, setIsLoading] = useState(false);
   const submitParams = async () => {
     const res = await fetch("/api/rover-photos", {
       method: "POST",
@@ -26,9 +28,13 @@ export default function Home() {
       headers: {
         "Content-Type": "application/json",
       },
+      next: { revalidate: 60 },
     });
     const result = await res.json();
-    //console.log(result.data.photos);
+    //todo solve api limit issue
+    setSolArrayLength(result.data.photos.length);
+    setIsLoading(false);
+    console.log(result);
     setLoadedRoverData(result.data.photos);
   };
 
@@ -96,63 +102,81 @@ export default function Home() {
               size="md"
               onPress={(e) => {
                 submitParams();
+                setIsLoading(true);
               }}
             >
-              Search
+              {isLoading ? (
+                <Loading color="currentColor" size="sm" />
+              ) : (
+                "Search"
+              )}
             </Button>
           </Navbar.Item>
         </Navbar.Content>
       </Navbar>
 
       <Grid.Container gap={2} justify="center">
-        {loadedRoverData.map((photo) => {
-          return (
-            <Grid key={photo.id} xs={12} sm={4}>
-              <Card isPressable>
-                <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
-                  <Col>
-                    <Text
-                      size={16}
-                      weight="bold"
-                      transform="uppercase"
-                      color="#ffffffAA"
-                    >
-                      {photo.rover.name}
-                    </Text>
-                    <Text h4 color="white">
-                      Date: {photo.earth_date}
-                    </Text>
-                  </Col>
-                </Card.Header>
-                <Card.Image
-                  src={photo.img_src}
-                  objectFit="cover"
-                  width="100%"
-                  height={400}
-                  alt="rover image"
-                />
-              </Card>
-            </Grid>
-          );
-        })}
+        {solArrayLength == 0 ? (
+          <div>
+            <Container>
+              <Text h3>Whoops, could not found any Mars Rover photos</Text>
+              <Text h4>Enter a solar day to search for solar days</Text>
+            </Container>
+          </div>
+        ) : (
+          loadedRoverData.map((photo) => {
+            return (
+              <Grid key={photo.id} xs={12} sm={4}>
+                <Card
+                  isPressable
+                  onPress={(e) => {
+                    console.log();
+                  }}
+                >
+                  <Card.Header
+                    css={{ position: "absolute", zIndex: 1, top: 5 }}
+                  >
+                    <Col>
+                      <Text
+                        size={16}
+                        weight="bold"
+                        transform="uppercase"
+                        color="#ffffffAA"
+                      >
+                        {photo.rover.name}
+                      </Text>
+                      <Text h4 color="white">
+                        Date: {photo.earth_date}
+                      </Text>
+                    </Col>
+                  </Card.Header>
+                  <Card.Image
+                    src={photo.img_src}
+                    objectFit="cover"
+                    width="100%"
+                    height={400}
+                    alt="rover image"
+                  />
+                </Card>
+              </Grid>
+            );
+          })
+        )}
       </Grid.Container>
     </>
   );
 }
 
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   const NASA_API_KEY = process.env.NASA_API_KEY;
-//   const NASA_API_URL = "https://api.nasa.gov/mars-photos/api/v1";
-//   const rover = "curiosity";
-//   const solarDay = 1000;
-
-//   const res = await fetch(
-//     `${NASA_API_URL}/rovers/${rover}/photos?sol=${solarDay}&api_key=${NASA_API_KEY}`
-//   );
-
-//   const data = await res.json();
-
-//   return {
-//     props: { data }, // will be passed to the page component as props
-//   };
-// };
+Home.getInitialProps = async (ctx) => {
+  const NASA_API_KEY = process.env.NASA_API_KEY;
+  const NASA_API_URL = "https://api.nasa.gov/mars-photos/api/v1";
+  const rover = "curiosity";
+  const sol = "0";
+  const loadedData = await fetch(
+    `${NASA_API_URL}/rovers/${rover}/photos?sol=${sol}&api_key=${NASA_API_KEY}`
+  );
+  const initialRoverData = await loadedData.json();
+  return {
+    initialRoverData: initialRoverData.photos,
+  };
+};
